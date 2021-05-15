be9be5b2547b5b293421bdce1439ba65eac5f925
HouKunLin <houkunlin@aliyun.com>
2021-05-15 18:43:36 +0800

    feat: LambdaQuery add QueryChainWrapper method

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 [![](https://jitpack.io/v/houkunlin/dao-extend-starter.svg)](https://jitpack.io/#houkunlin/dao-extend-starter)
+[![](https://jitci.com/gh/houkunlin/dao-extend-starter/svg)](https://jitci.com/gh/houkunlin/dao-extend-starter)
 
 # DAO 扩展
--- src/main/java/com/houkunlin/dao/extend/mybatisplus/LambdaQuery.java
+++ src/main/java/com/houkunlin/dao/extend/mybatisplus/LambdaQuery.java
@@ -1,9 +1,13 @@
 package com.houkunlin.dao.extend.mybatisplus;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
 import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
 import java.util.function.BiConsumer;
 
 /**
@@ -16,7 +20,34 @@ public interface LambdaQuery<T> {
      * @param wrapper LambdaQueryChainWrapper
      * @return LambdaQueryChainWrapper
      */
-    LambdaQueryChainWrapper<T> queryBuilder(LambdaQueryChainWrapper<T> wrapper);
+    default LambdaQueryChainWrapper<T> queryBuilder(LambdaQueryChainWrapper<T> wrapper) {
+        return wrapper;
+    }
+
+    /**
+     * 查询条件构建
+     *
+     * @param wrapper QueryChainWrapper
+     * @return QueryChainWrapper
+     */
+    default QueryChainWrapper<T> queryBuilder(QueryChainWrapper<T> wrapper) {
+        return wrapper;
+    }
+
+    /**
+     * 排序参数构建
+     *
+     * @param wrapper QueryChainWrapper
+     * @param page    分页
+     * @return QueryChainWrapper
+     */
+    default QueryChainWrapper<T> orderBuilder(QueryChainWrapper<T> wrapper, IPage<T> page) {
+        final List<OrderItem> orders = page.orders();
+        for (final OrderItem order : orders) {
+            wrapper.orderBy(true, order.isAsc(), order.getColumn());
+        }
+        return wrapper;
+    }
 
     /**
      * 增加查询条件
@@ -33,12 +64,47 @@ public interface LambdaQuery<T> {
     }
 
     /**
+     * 增加查询条件
+     *
+     * @param field 需要查询的字段
+     * @param value 查询的值
+     * @param build QueryChainWrapper 的查询条件方法
+     */
+    default void addQuery(String field, Object value, BiConsumer<String, Object> build) {
+        if (value != null) {
+            build.accept(field, value);
+        }
+    }
+
+    /**
      * 查询条件构建
      *
      * @param service IService<T>
      * @return LambdaQueryChainWrapper<T>
      */
-    default LambdaQueryChainWrapper<T> queryBuilder(IService<T> service) {
+    default LambdaQueryChainWrapper<T> lambdaQuery(IService<T> service) {
         return queryBuilder(service.lambdaQuery());
+    }
+
+    /**
+     * 查询条件构建
+     *
+     * @param service IService<T>
+     * @return LambdaQueryChainWrapper<T>
+     */
+    default QueryChainWrapper<T> query(IService<T> service) {
+        return queryBuilder(service.query());
+    }
+
+    /**
+     * 查询条件构建
+     *
+     * @param service IService<T>
+     * @param orders  IPage<T>
+     * @return LambdaQueryChainWrapper<T>
+     */
+    default QueryChainWrapper<T> query(IService<T> service, IPage<T> orders) {
+        final QueryChainWrapper<T> queryBuilder = queryBuilder(service.query());
+        return orderBuilder(queryBuilder, orders);
     }
 }
